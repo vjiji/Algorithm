c4d4af96a448a412057bd06c2b1f87b7fead7b58
vjiji <155742212+vjiji@users.noreply.github.com>
2024-02-14 09:19:57 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.66 ms, Memory: 35.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,15 @@
+function cal(num) {
+  let count = 0;
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) count += 1;
+  }
+  return count;
+}
+
+function solution(left, right) {
+  let sum = 0;
+  for (let i = left; i < right + 1; i++) {
+    cal(i) % 2 ? (sum -= i) : (sum += i);
+  }
+  return sum;
+}
