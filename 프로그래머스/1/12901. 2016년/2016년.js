7fd5d30cf0082fca4ca3b0d1ef5ad8bc87ebe010
vjiji <155742212+vjiji@users.noreply.github.com>
2024-02-16 11:30:00 +0900

    [level 1] Title: 2016년, Time: 0.25 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,8 +1,5 @@
 function solution(a, b) {
-  const DAY_NAME = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
-  const monthDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  const fullDays =
-    a === 1 ? 0 : monthDays.slice(0, a - 1).reduce((acc, cur) => acc + cur);
-  const totalDays = fullDays + b;
-  return DAY_NAME[totalDays % 7];
+    var tempDate = new Date(2016, a-1, b);
+
+    return tempDate.toString().slice(0,3).toUpperCase();
 }
