944c641fa12a013d6e340f33c7fbc30522df365e
vjiji <155742212+vjiji@users.noreply.github.com>
2024-02-24 09:58:26 +0900

    [level 1] Title: 폰켓몬, Time: 0.26 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,9 +1,6 @@
 function solution(nums) {
-  let count = [];
-  nums
-    .sort((a, b) => a - b)
-    .forEach((num) =>
-      count[count.length - 1] !== num ? count.push(num) : false
-    );
-  return count.length < nums.length / 2 ? count.length : nums.length / 2;
+  const max = nums.length / 2;
+  const arr = [...new Set(nums)];
+
+  return arr.length > max ? max : arr.length
 }
